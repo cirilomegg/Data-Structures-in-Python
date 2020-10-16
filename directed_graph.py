4e84e056d967e7d4cc50492da42db25b3dca2145
Cirilo Meggiolaro <cirilomegg@hotmail.com>
2020-10-16 19:24:34 -0400

    Add script for a directed graph.

---
+++ directed_graph.py
@@ -0,0 +1,29 @@
+class Edge:
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+
+
+class Graph:
+    def __init__(self, edges):
+        self.dict = {}
+
+        for current in edges:
+            if not current.source in self.dict:
+                self.dict[current.source] = []
+
+            self.dict[current.source].append(current.destination)
+
+    def print(self):
+        for src in self.dict:
+            for dest in self.dict[src]:
+                print(f"({src} -> {dest}) ", end="")
+            print()
+
+
+if __name__ == "__main__":
+    edges = [Edge(0, 1), Edge(1, 2), Edge(2, 0), Edge(2, 1),
+             Edge(3, 2), Edge(4, 5), Edge(5, 4)]
+
+    graph = Graph(edges)
+    graph.print()
